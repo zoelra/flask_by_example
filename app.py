c0fb139b9d5000e545f00e1e6b24a17d654bc07a
Zophia Raleigh <zophiaraleigh1@gmail.com>
2019-01-19 22:25:59 -0600

    Updated duplicate app route

--- app.py
+++ app.py
@@ -13,10 +13,5 @@ def hello_name(name):
     return "Hello {}!".format(name)
 
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
-
-
 if __name__ == '__main__':
     app.run()
