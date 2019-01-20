478892ff23d5a096acfcf8389b601fec3176974d
Zophia Raleigh <zophiaraleigh1@gmail.com>
2019-01-19 22:18:04 -0600

    Updated 'app.py' and '.env' and created 'config.py'

--- app.py
+++ app.py
@@ -12,5 +12,11 @@ def hello():
 def hello_name(name):
     return "Hello {}!".format(name)
 
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+
 if __name__ == '__main__':
     app.run()
